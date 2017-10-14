7b4719b923cba30e00ceb3b9638ec18320ae62f1
kristenspirka12 <krispi98@gmail.com>
2017-10-14 18:36:11 +0300

    Add files via upload

---
+++ src/KoduneÜlesanne.java
@@ -0,0 +1,10 @@
+
+public class KoduneÜlesanne {
+
+	public static void main(String[] args) {
+		Random generate = new Random();
+	       String[] name = {"Peeter", "Daniel", "Karl", "Hendrik", "Oliver", };
+
+	}
+
+}
