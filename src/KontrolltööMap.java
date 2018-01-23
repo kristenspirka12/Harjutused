b544294f065b179a5ef30f055e52b0cf8b0c4bc1
kristenspirka12 <krispi98@gmail.com>
2018-01-23 21:16:55 +0200

    Add files via upload

--- src/KontrolltööMap.java
+++ src/KontrolltööMap.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class KontrolltööMap {
 
@@ -14,12 +15,12 @@ public class KontrolltööMap {
 		map1.put(3, "aabits");
 
 		System.out.println(kt.keysStartsA(map1));
-		
-		
+		// System.out.println(kt.keysStartsA(map1));
+
 		List<String> isikukoodid = new ArrayList<>();
 		isikukoodid.add("39001247777");
 		isikukoodid.add("48501241234");
-		
+
 		// => [ {"39001247777" : "mees"}, {"48501241234" : "naine"}]
 	}
 
@@ -33,20 +34,35 @@ public class KontrolltööMap {
 		}
 		return keys;
 	}
-	
+
 	// Ülesanne 2
 	public Map<String, String> mapCodes(List<String> codes) {
 		// luua map, kuhu lähevad isikukoodid koos "mees"/"naine"
 		Map<String, String> map = new HashMap<String, String>();
 		// FOR-loop codes
-		
-		
-		return null;
+		for (Entry<String, String> entry : map.entrySet()) {
+
+		}
+		return map;
 	}
-	
+
 	private String getSex(String code) {
-		Integer firstNumber = Integer.parseInt(code.substring(0,1));
+		Integer firstNumber = Integer.parseInt(code.substring(0, 1));
 		boolean isEven = firstNumber % 2 == 0;
 		return isEven ? "naine" : "mees";
 	}
+
+	// Ülesanne 3
+	// Eemalda kõik paarid, mille väärtus on suurem kui 10.
+	// public Map<Integer, String> biggerThan10 = new HashMap<Integer, String>();
+
+	// Ülesanne 4
+	// Muuta etteantud mapi nii, et kui mõlemad võtmed "a" ja "b" on esindatud, siis eemaldatakse võti-väärtuspaar "c",
+	// kui see on olemas. Kui "c" olemas ei ole, siis luua paar "d" :"a ja b puuduvad".
+
+	// Ülesanne 5
+	// Antud massiiv ja täht. Meetodi ülesandeks on luua map, kus massiivi elemendid
+	// on mapi võtmed ja väärtuseks on etteantud tähe esinemise arv antud sõnas.
+	// Võib teha eraldi abimeetodi sõnas vastava tähe esinemise kordade kohta.
+
 }
