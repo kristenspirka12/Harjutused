cbac35c32941e75980ad22a7c48f071c3baa7d1b
kristenspirka12 <krispi98@gmail.com>
2018-02-28 11:53:17 +0200

    student

--- src/App.java
+++ src/App.java
@@ -1,22 +1,28 @@
+package student;
+
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class App {
 
 	public static void main(String[] args) {
-		Headphone korvaKlapid1 = new Headphone("Bludio", 50, false);	
-		System.out.println(korvaKlapid1);
-		System.out.println("Kas on kallis: " + korvaKlapid1.isExpensiveString());
-		System.out.println(korvaKlapid1.isExpensive() ? "Kõrvaklapid on kallid" : "Kõrvaklapid on odavad");
+		List<Student> students = new ArrayList<Student>();
 		
-		Headphone korvaKlapid2 = new Headphone("Samsung");
-		korvaKlapid2.setPrice(154.50);
-		System.out.println(korvaKlapid2);
+		Student mati = new Student("Mati", LocalDate.parse("2017-01-05"), Type.TUDENG);
+		mati.addSubject(Subject.BIOLOOGIA);
+		mati.addSubject(Subject.FÜÜSIKA);
+		//System.out.println(mati);
 		
-		List<String> specs = new ArrayList<>(Arrays.asList("mikrofon", "mürasummutaja"));
-		Headphone korvaklapid3 = new Headphone("Sony", 180, false, specs);
-		korvaklapid3.addSpecification("NFC");
-		System.out.println(korvaklapid3);
+		students.add(mati);
+		students.add(new Student("Kati", LocalDate.parse("1986-03-08"), Type.LÕPETAJA));
+		
+		Student vello = new Student("Vello", LocalDate.parse("1980-01-05"), Type.LÕPETAJA, "38001055555", Country.RUSSIA);
+		students.add(vello);
+		
+		for (Student student : students) {
+			System.out.println(student);
+		}
+
 	}
 }
